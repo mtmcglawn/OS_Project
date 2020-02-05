7a94d1cc5a15681ca8baec55e7ad532c2a5281e8
arm0086 <60526460+arm0086@users.noreply.github.com>
2020-02-04 19:19:03 -0600

    changes
    add pseudo code

--- FileSystem.c
+++ FileSystem.c
@@ -1,19 +1,28 @@
 #include <stdio.h> 
 #include <stdbool.h>
+FILE *fptr;
 
-//Erase All Sectors (sets all bits in simlates memory to value 1)
+//Erase All Sectors sets all bits in simlates memory to value 1
 // if necessary, creates the file simulating the medium
 bool EraseAllSectors() {
-	if () { //if file does not 
-		//create file
-	}
+	fptr = fopen("memory.bin","wb+");
 	for(int i= 0; i<20; i++){
 		EraseSector(i);
 	}
+	return 1;
+}
+
+//Read Word: Reads a word (2 bytes) from specific address
+ReadWord (nAddress) {
+
+}
+//Write Word: Writes a WORD (2bytes) to spefic address
+WriteWord (nAddress, nWord) {
+	
 }
 
 
-
-int main(){       
+int main(){ 
+EraseAllSectors();      
 return 0;   
 }  
