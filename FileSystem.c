edf698ff3c25de34879b21235ba4b759127b3fac
arm0086 <60526460+arm0086@users.noreply.github.com>
2020-02-04 18:24:27 -0600

    Update FileSystem.c

--- FileSystem.c
+++ FileSystem.c
@@ -1,5 +1,19 @@
 #include <stdio.h> 
-#include <stdbool.h>   
+#include <stdbool.h>
+
+//Erase All Sectors (sets all bits in simlates memory to value 1)
+// if necessary, creates the file simulating the medium
+bool EraseAllSectors() {
+	if () { //if file does not 
+		//create file
+	}
+	for(int i= 0; i<20; i++){
+		EraseSector(i);
+	}
+}
+
+
+
 int main(){       
 return 0;   
 }  
