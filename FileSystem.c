268bb9b4f495eddd7dd45493f590ff12e14d222e
Matthew McGlawn <mtm0002@auburn.edu>
2020-02-16 02:52:05 -0600

    Working Code
    The code now runs correctly, I went through and made comments and made it look "professional". We still need to check for edge cases, including trying to read and write to a file that doesn't exist.

--- FileSystem.c
+++ FileSystem.c
@@ -1,9 +1,25 @@
 #include <stdio.h> 
 #include <stdbool.h>
+
+/*
+Program to create and manipulate a .bin file that represents
+	memory.
+
+@author James Barr
+@author Chris Flodstrom
+@author Eliza Hall
+@author Matthew McGlawn
+@author	Wesley Monnette
+@author Alexa Monroe
+
+@version 2020-02-16
+*/
+
 FILE *fptr;
 
-static int SECTOR_SIZE = 64000; //In bytes
-static int FILE_SIZE = 1280000; //In bytes
+static int SECTORS = 20;
+static long int SECTOR_SIZE = 64000; //In bytes
+static long int FILE_SIZE = 1280000; //In bytes
 
 struct word {
 	unsigned char byte1;
@@ -15,105 +31,181 @@ struct word ReadWord(long nAddress);
 bool WriteWord(long nAddress, struct word nWord);
 bool EraseSector(int Sect);
 
-//Erase All Sectors sets all bits in simlates memory to value 1
-// if necessary, creates the file simulating the medium
+/*
+Erase All Sectors sets all bits in simlates memory to value 1
+if necessary, creates the file simulating the medium.
 
-bool EraseAllSectors() {
+@return bool value upon proper completion.
+*/
+bool EraseAllSectors(){
+
+	//Opens or creates file
 	fptr = fopen("memory.bin","wb+");
-	for(int i= 0; i<20; i++){
+
+	//Loops through all sectors and Erases them
+	for(int i = 0; i < SECTORS; i++){
 		EraseSector(i); 
 	}
 	return 1;
 }
 
-//Read Word: Reads a WORD (2 bytes) from specific address
-struct word ReadWord (long nAddress) {
+/*
+Reads a WORD (2 bytes) from the specified address. Address should
+be on the WORD boundary, i.e., addresses 0, 2, …, 2n are valid, 
+addresses 1, 3, …, 2n-1 are not. If specified address is not WORD
+aligned or is outside the size of the simulated medium, this
+operation should fail.
+
+@param nAddress – address of the WORD to be read. Address is the
+	offset from the beginning of the simulated memory (i.e., byte 
+	0 in Sector 0) and not from the particular sector.
+@return struct that contains the Word at the address.
+*/
+struct word ReadWord(long nAddress){
+
 	struct word result;
-	char storage[2];
-	//checks that address is on boundary
-	if ((nAddress % 2) != 0) {
+	unsigned char storage[2];
+
+	//Checks that address is on WORD boundary
+	if((nAddress % 2) != 0){
 		printf("Address must be on WORD boundary.(i.e. even index)");
 		return result;
 	}
-	//makes sure file can be opened
+
+	//Makes sure file can be opened
 	fptr = fopen("memory.bin","rb");
-	if (fptr == NULL) {
+	if(fptr == NULL){
 		printf("Unable to open file.");
 		return result;
 	}
-	//calculates size of file 
-	long size = SECTOR_SIZE * FILE_SIZE;
-	if ((size < nAddress) || nAddress < 0)  {
+
+	//Compares the Address to the file size
+	if((FILE_SIZE < nAddress) || nAddress < 0){
 		printf("Address out of file size bounds.");
 		return result;
 	}
-	fseek(fptr, nAddress, SEEK_SET);
-	fgets(storage, 3, fptr);
-	fclose(fptr);
-	//puts specified bytes into word structure
+
+	//Goes to the location and retrieves the WORD
+	fseek(fptr, nAddress, SEEK_SET);//Goes to address
+	fgets(storage, 3, fptr);//Changes value
+	fclose(fptr);//Closes file
+
+	//Puts specified bytes into word structure
 	result.byte1 = storage[0];
 	result.byte2 = storage[1];
+
 	return result;
 }
 
-//Write Word: Writes a WORD (2bytes) to spefic address
+/*
+Writes a WORD (2 bytes) to the specified address. If specified
+address is not WORD aligned or is outside the size of the simulated
+memory, this operation should fail, i.e., no information should be
+written in the simulated memory. If address is valid, the value
+written in the specified address should be equal to nWord AND (bitwise)
+ReadWord (nAddress) Whether this function returns value or not and
+what is the meaning of the returned value is a decision you should
+make.
+
+@param nAddress – address in which the WORD should be written.
+@param nWord – WORD to be written in the specified address.
+@return bool value discerning between proper completion or failure.
+*/
 bool WriteWord(long nAddress, struct word nWord){
-	if ((nAddress % 2) != 0) {
+
+	//Checking for WORD boundary
+	if((nAddress % 2) != 0){
 		printf("Address must be on WORD boundary.(i.e. even index)");
 		return 0;
 	}
 
+	struct word rWord = ReadWord(nAddress);//Gets the current value
+	
+	//Opening the file to manipulate
+	fptr = fopen("memory.bin", "rb+");
 	if (fptr == NULL){
 		printf("Unable to open file!");
 		return 0;
 	}
 
-	FILE *fptr = fopen("memory.bin", "wb");
-	struct word rWord = ReadWord(nAddress);
+	//Bitwise AND to calculate the new value
+	unsigned char byteOne = rWord.byte1 & nWord.byte1;
+	unsigned char byteTwo = rWord.byte2 & nWord.byte2;
 
-	char byteOne = rWord.byte1 & nWord.byte1;
-	char byteTwo = rWord.byte2 & nWord.byte2;
+	struct word finalWord = {byteOne, byteTwo};//word creation
 
-	struct word finalWord = {byteOne, byteTwo};
+	//Replacing the current WORD value with the new WORD
+	fseek(fptr, nAddress, SEEK_SET);//Goes to address
+	fwrite(&finalWord, sizeof(struct word), 1, fptr);//Changes value
+	fclose(fptr);//Closes file
 
-	fseek(fptr, nAddress, SEEK_SET);
-	fwrite(&finalWord, sizeof(struct word), 1, fptr);
-	fclose(fptr);
 	return 1;
 }
 
-//Given a Sect from 0-19 toggle all bits in that sector to 1
-bool EraseSector(int Sect) {
-	FILE *fptr;
-	static struct word allOnes = {0xFF, 0xFF};
+/*
+Sets all bits in the specified sector to 1. Sector numbers are 
+0-19. If file simulating simulated memory is not present,
+EraseAllSectors() is executed first.
 
-	//Program exits if the file pointer returns NULL.
+@param nSectorNr - int containing the sector to erase.
+@return bool representing successful completion.
+*/
+bool EraseSector(int nSectorNr){
+
+	static struct word allOnes = {0xFF, 0xFF};//"Erased" word
+
+	//Program calls EraseAllSectors if NULL to create new file
 	fptr = fopen("memory.bin","rb+");
 	if(fptr == NULL){
 		EraseAllSectors();
 	}
 
 	//Moves file pointer to selected sector
-	fseek(fptr, (Sect * SECTOR_SIZE), SEEK_SET);
-	//Wites the word allOnes to each word in the sector filling it with 1s.
+	fseek(fptr, (nSectorNr * SECTOR_SIZE), SEEK_SET);
+
+	//Wites the word allOnes to each word in the sector filling it
 	for(int i = 0; i < 32000; i++){
 		fwrite(&allOnes, sizeof(struct word), 1, fptr);
 	}
-	//Closes file
-	fclose(fptr);
+	fclose(fptr);//Closes file
 
 	return 0;
 }
 
+/*
+Function used for testing purposes
+*/
 int main(){
-	long placement = 0;
-	EraseAllSectors();
+	long placement = 4;//place to write
+	unsigned char testA = 0xAA;//AA = 1010 1010
+	unsigned char testB = 0xAA;//AA = 1010 1010
+	unsigned char inFileA = 0xFF;//FF = 1111 1111
+	unsigned char inFileB = 0xFF;//FF = 1111 1111
+
+	//ensures the file has been created
+	EraseAllSectors();//Makes file All "FF"
+
+	//Gets original value to compare against
 	struct word test = ReadWord(placement);
-	struct word test1 = {0xAA, 0xAA};//AA=10101010
-	//WriteWord(placement, test1);
+	
+	//Writes the word
+	struct word test1 = {testA, testB};//word to be written
+	WriteWord(placement, test1);
+
+	//Gets new value to compare against
 	struct word test2 = ReadWord(placement);
-	printf("%X%X\n", test.byte1, test.byte2);
-	printf("%X%X\n", test1.byte1, test1.byte2);
-	printf("%X%X\n", test2.byte1, test2.byte2);
+
+	//shows original value 
+	printf("%X%X\n", inFileA, inFileB);//Actual
+	printf("%X%X\n", test.byte1, test.byte2);//Actual
+
+	//shows value to be written
+	printf("%X%X\n", testA, testB);//Expected
+	printf("%X%X\n", test1.byte1, test1.byte2);//Actual
+
+	//shows returned value
+	printf("%X%X\n", testA & inFileA, testB & inFileB);//Expected
+	printf("%X%X\n", test2.byte1, test2.byte2);//Actual
+	
 	return 0;
 }  
